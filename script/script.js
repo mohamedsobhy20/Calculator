1e10294ad4080c0bc2efbc1a4fe8eccf03c66ee7
Sobhy-PC\Sobhy <sobhymohamed482@yahoo.com>
2017-04-09 19:21:01 +0200

    appling some best practice rules .

--- script/script.js
+++ script/script.js
@@ -1,36 +1,37 @@
+//using strict mode
+"use strict";
+
 //add event listeners to  buttons on window load
 window.onload = function () {
-	var operationsArray = ["plus-btn","min-btn","mul-btn","divison_btn"]
-						for (i=0 ; i < operationsArray.length ; i++) {
-						document.getElementById(operationsArray[i]).addEventListener("click", replace);
-						}
-	
-	var buttonsArray = ["plus-btn","min-btn","mul-btn","divison_btn","dot-btn","zero","one","two",
-						"three","four","five","six","seven","eight","nine","plus-btn","min-btn"
-						,"mul-btn","divison_btn","clear"]
-						for (i=0 ; i < buttonsArray.length ; i++) {
-						document.getElementById(buttonsArray[i]).addEventListener("click", inputValue);
-						}
+    var i;
 
-	document.getElementById("dot-btn").addEventListener("click", singleDot);
-	document.getElementById("delete").addEventListener("click", removeLast);
-	document.getElementById("result").addEventListener("click", result);
+    var operations_array = [ "plus-btn","min-btn","mul-btn","division_btn" ];
+        for ( i = 0; i < operations_array.length; i++ ) {
+            document.getElementById(operations_array[i]).addEventListener( "click", replace );
+        }
+
+    var buttons_array = [ "plus-btn","min-btn","mul-btn","division_btn","dot-btn","zero","one","two",
+						"three","four","five","six","seven","eight","nine","plus-btn","min-btn",
+                        "mul-btn","division_btn","clear" ];
+        for ( i = 0; i < buttons_array.length; i++ ) {
+		    document.getElementById(buttons_array[i]).addEventListener( "click", input_value );
+        }
+
+	document.getElementById("dot-btn").addEventListener( "click", single_dot );
+	document.getElementById("delete").addEventListener( "click", remove_last );
+	document.getElementById("result").addEventListener( "click", result );
 };
-
-// global variables
-// signs array
-var signs = ["-", "+", "÷", "×"];
 
 /**
  * function to remove the last letter
  *
  * @return void
  */
-function removeLast() {
+function remove_last() {
 	// calculator screen value
 	var calc_screen = document.getElementById("demo").innerHTML;
 	//delete the last letter
-	document.getElementById("demo").innerHTML = calc_screen.substring(0, calc_screen.length - 1);
+	document.getElementById("demo").innerHTML = calc_screen.substring( 0, calc_screen.length - 1 );
 }
 
 /**
@@ -38,16 +39,17 @@ function removeLast() {
  *
  * @return void
  */
-function inputValue() {
-	// get btn val
-	var input = this.value;
-	// calculator screen value
-	var calc_screen = document.getElementById("demo").innerHTML;
-	// not to start array
-	var notStart = ["+", "÷", "×"];
+function input_value() {
+
+	var input, calc_screen, signs, not_start;
+
+        input = this.value; // get btn val
+	    calc_screen = document.getElementById("demo").innerHTML; // calculator screen value
+	    signs = ["-", "+", "÷", "x"]; // signs array
+	    not_start = ["+", "÷", "x"]; // not to start array
 
 	// not to write (+ or ÷ or x ) in the beginning and clear button
-	if (!calc_screen && notStart.indexOf(input) >= 0 || !input) {
+	if ( ! calc_screen && not_start.indexOf(input) >= 0 || ! input ) {
 		document.getElementById("demo").innerHTML = "";
 
 		//write the input in the screen
@@ -57,7 +59,7 @@ function inputValue() {
 
 	// negative sign in the beginning
 	calc_screen = calc_screen + input;
-	if (calc_screen[0] === "-" && signs.indexOf(input) >= 0) {
+	if ( calc_screen[0] === "-" && signs.indexOf(input) >= 0 ) {
 		document.getElementById("demo").innerHTML = "-";
 	}
 }
@@ -68,12 +70,15 @@ function inputValue() {
  * @return void
  */
 function replace() {
-	var input = this.value;
-	// calculator screen value
-	var calc_screen = document.getElementById("demo").innerHTML;
+	var input, calc_screen, signs;
+
+        input = this.value; // get btn val
+	    calc_screen = document.getElementById("demo").innerHTML; // calculator screen value
+	    signs = [ "-", "+", "÷", "x" ]; // signs array
+
 	// deleting last letter if this letter is a sign and the input is a sign also.
-	if (signs.indexOf(calc_screen[calc_screen.length - 1]) >= 0 && signs.indexOf(input) >= 0) {
-		removeLast();
+	if (signs.indexOf(calc_screen[ calc_screen.length - 1 ]) >= 0 && signs.indexOf(input) >= 0) {
+		remove_last();
 	}
 
 	// add 0 to last char if . exists
@@ -88,16 +93,15 @@ function replace() {
  *
  * @return void
  */
-function singleDot() {
-	// calculator screen value
-	var calc_screen = document.getElementById("demo").innerHTML;
-	// whole string after deleting the last letter
-	var deleteLast = calc_screen.substring(0, calc_screen.length - 1);
-	// signs length
-	var length = signs.length;
-	var c = 0;
-	// variable to calculate the last index for last element from signs array
-	var last_index = 0;
+function single_dot() {
+
+	var calc_screen, signs, length, c, last_index;
+
+        calc_screen = document.getElementById("demo").innerHTML; // calculator screen value
+        signs = [ "-", "+", "÷", "x"]; // signs array
+	    length = signs.length; // signs length
+	    c = 0; // random variable to store the last index value in while loop
+        last_index = 0; // variable to store the last index for last element from signs array
 
 	while (length > 0) {
 
@@ -120,19 +124,23 @@ function singleDot() {
 
 		if (calc_screen.lastIndexOf(".") > calc_screen.indexOf(".")) {
 		
-			 removeLast();
+			 remove_last();
 		}
 
 	} else {
-		// all written letters after last sign
-		var afterSign = calc_screen.slice(last_index + 1, calc_screen.length);
-		// all written letters before last sign containing the sign
-		var beforeSign = calc_screen.slice(0, last_index + 1);
-		// all written letters after sign but with deleteing the input
-		var deleteLastSign = afterSign.substring(0, afterSign.length - 1);
+		var after_sign, delete_last_sign ;
 
-		if (afterSign.lastIndexOf(".") > afterSign.indexOf(".")) {
-			document.getElementById("demo").innerHTML = beforeSign + deleteLastSign;
+        // all written letters after last sign
+		  after_sign = calc_screen.slice(last_index + 1, calc_screen.length);
+        // all written letters after sign but with deleting the input
+        delete_last_sign = after_sign.substring(0, after_sign.length - 1);
+
+        // all written letters before last sign containing the sign
+		//  calc_screen.slice(0, last_index + 1);
+
+
+		if (after_sign.lastIndexOf(".") > after_sign.indexOf(".")) {
+			document.getElementById("demo").innerHTML = calc_screen.slice(0, last_index + 1) + delete_last_sign;
 		}
 	}
 }
@@ -146,7 +154,7 @@ function singleDot() {
 function calculate() {
 	// calculator screen value
 	var calc_screen = document.getElementById("demo").innerHTML;
-	document.getElementById("demo").innerHTML = eval((calc_screen.replace("×", "*")).replace("÷", "/"));
+	document.getElementById("demo").innerHTML = eval( (calc_screen.replace( "x","*" )).replace( "÷", "/" ) );
 
 }
 
@@ -156,18 +164,19 @@ function calculate() {
  * @return void
  */
 function result() {
-	// calculator screen value
-	var calc_screen = document.getElementById("demo").innerHTML;
-	// variable to find the last letter
-	if (signs.indexOf(calc_screen[calc_screen.length - 1]) >= 0) {
-		// if the last letter is a sign , deleting the last letter then calculate
-		removeLast();
+    var calc_screen, signs;
+
+	calc_screen = document.getElementById("demo").innerHTML; // calculator screen value
+	signs = [ "-", "+", "÷", "x" ]; // signs array
+
+    // if the last letter is a sign , deleting the last letter then calculate
+    if (signs.indexOf(calc_screen[calc_screen.length - 1]) >= 0) {
+		remove_last();
 		calculate();
 	} else {
 		calculate();
 	}
-	if (!calc_screen) {
-		//if the screen is empty
+	if (! calc_screen) { //if the screen is empty
 		document.getElementById("demo").innerHTML = "";
 	}
 
